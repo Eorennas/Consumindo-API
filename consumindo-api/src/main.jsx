17f0d606c602dccb3e8f77b510bd4b08311cde93
Eorennas <141781304+Eorennas@users.noreply.github.com>
2024-08-19 15:06:37 -0300

    atualizando

--- consumindo-api/src/main.jsx
+++ consumindo-api/src/main.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+import Home from './routers/home.jsx';
+import NewPost from './routers/NewPost.jsx'
+import App from './App.jsx';
+import './index.css';
+
+const router = createBrowserRouter([
+  {
+    element: <App />, 
+    children: [
+      {
+        path: '/',
+        element: <Home />
+      },
+      {
+        path: '/new',
+        element: <NewPost/ >
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} /> 
   </StrictMode>,
-)
+);
